d3ac5e2314b118d1e23d41317a3ee4d345796e12
eknuviad <edemnuviadenu@gmail.com>
2019-09-14 02:29:43 -0400

    BangBangController sample implementation

--- ecse211-lab1/src/ca/mcgill/ecse211/lab1/BangBangController.java
+++ ecse211-lab1/src/ca/mcgill/ecse211/lab1/BangBangController.java
@@ -4,6 +4,11 @@ import static ca.mcgill.ecse211.lab1.Resources.*;
 
 public class BangBangController extends UltrasonicController {
 
+  public static final int WALLDIST = 20; //Standoff distance to wall/ bandcenter
+  public static final int DEADBAND = 2;  //Error threshold or bandwidth
+  public static final int DELTASPD = 100; //Bang-bang constant
+  public static final int SLEEPINT = 50;  //Sleep interval 50ms = 20Hz
+  
   public BangBangController() {
     LEFT_MOTOR.setSpeed(MOTOR_HIGH); // Start robot moving forward
     RIGHT_MOTOR.setSpeed(MOTOR_HIGH);
@@ -16,6 +21,32 @@ public class BangBangController extends UltrasonicController {
     filter(distance);
 
     // TODO: process a movement based on the us distance passed in (BANG-BANG style)
+    int errorDist = WALLDIST - distance; //compute error
+    
+    if(Math.abs(errorDist) <= DEADBAND) {
+      LEFT_MOTOR.setSpeed(MOTOR_HIGH);
+      RIGHT_MOTOR.setSpeed(MOTOR_HIGH);
+      LEFT_MOTOR.forward();
+      RIGHT_MOTOR.forward();
+    }
+    else if(errorDist > 0) {
+      LEFT_MOTOR.setSpeed(MOTOR_HIGH + DELTASPD);
+      RIGHT_MOTOR.setSpeed(MOTOR_HIGH - DELTASPD);
+      LEFT_MOTOR.forward();
+      RIGHT_MOTOR.forward();  
+    }
+    else if(errorDist < 0) {
+      LEFT_MOTOR.setSpeed(MOTOR_HIGH - DELTASPD);
+      RIGHT_MOTOR.setSpeed(MOTOR_HIGH + DELTASPD);
+      LEFT_MOTOR.forward();
+      RIGHT_MOTOR.forward();  
+    }
+    try {
+      Thread.sleep(SLEEPINT);
+    } catch (InterruptedException e) {
+      // TODO Auto-generated catch block
+      e.printStackTrace();
+    }
   }
 
   @Override
